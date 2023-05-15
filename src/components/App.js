975afcac16b91fc08e9c1543ea8543c6e2ef4127
sunxsoo <njs783075@gmail.com>
2023-05-15 17:50:46 +0900

    feat: About 화면 구현 (#9)

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import { Route, Routes } from 'react-router-dom';
 import Main from '../routes/Main';
+import About from '../routes/About';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Main />} />
+      <Route path="/about" element={<About />} />
     </Routes>
   );
 }
