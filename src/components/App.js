21b14a084ac34e7f91e1840346c63cf2f81ed135
sunxsoo <njs783075@gmail.com>
2023-05-22 03:49:59 +0900

    feat: repository 생성 및 인덱싱 연동 및 로딩화면 작업 (#7)

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import Main from '../routes/Main';
 import About from '../routes/About';
 import Contact from '../routes/Contact';
+import Result from '../routes/Result';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Route path="/" element={<Main />} />
       <Route path="/about" element={<About />} />
       <Route path="/contact" element={<Contact />} />
+      <Route path="/result" element={<Result />} />
     </Routes>
   );
 }
---
+++ src/components/Loading.js
@@ -0,0 +1,49 @@
+import React from 'react';
+
+const Loading = () => {
+  return (
+    <div
+      style={{
+        position: 'fixed',
+        top: 0,
+        left: 0,
+        width: '100%',
+        height: '100%',
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        zIndex: 9999,
+      }}
+    >
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+        <div
+          style={{
+            fontSize: '1.2rem',
+            color: '#fff',
+            marginBottom: '10px',
+          }}
+        >
+          Loading...
+        </div>
+        <div
+          style={{
+            width: '40px',
+            height: '40px',
+            border: '5px solid #fff',
+            borderRadius: '50%',
+            animation: 'loading 1.5s infinite ease-in-out',
+          }}
+        ></div>
+      </div>
+    </div>
+  );
+};
+
+export default Loading;
